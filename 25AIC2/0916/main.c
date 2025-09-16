6853ca16466cef47b54ce6d0a409dd12b1385da4
khn9513 <khn9513@gmail.com>
2025-09-16 17:42:34 +0900

    개같은 포인터

---
+++ 25AIC2/0916/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void sum(int x, int y, int* result)
+{
+	*result = x + y;
+}
+
+int main()
+{
+	int a = 3;
+	int b = 2;
+	int c = 0;
+		
+	sum(a, b, &c);
+
+	printf("%d", c);
+
+	return 0;
+}
+
